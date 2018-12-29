5ebc86b555be358a6169a97e6561a039449fdbb2
Andrew Trick <andyjtrick@gmail.com>
2018-12-28 18:39:04 -0600

    init py file for plotly.

---
+++ src/parallel_coordinates_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 28 18:26:48 2018
+
+@author: andyj
+"""
+import pandas as pd
+import plotly as py
+import plotly.graph_objs as go
+
+df = pd.read_csv('./data/test.csv')
