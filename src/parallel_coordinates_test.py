246b94af8a806a3196582210bee2234e288cffe0
Andrew Trick <andyjtrick@gmail.com>
2019-01-09 10:36:14 -0600

    color rehash.

--- src/parallel_coordinates_test.py
+++ src/parallel_coordinates_test.py
@@ -20,9 +20,9 @@ df = pd.read_csv('./data/test.csv')
 data = [
     go.Parcoords(
         line = dict(color = df['cat_num'],
-                   colorscale = [[0, '#CAD11A'], ## social services
-                                [0.25,'#6EBE18'], ## humanitarian
-                                [0.5,'#B71752'], ## health
+                   colorscale = [[0, '#6C9E12'], ## social services
+                                [0.25,'#0D5F67'], ## humanitarian
+                                [0.5,'#AA1B13'], ## health
                                 [0.75, '#69178C'], ## environmentla
                                 [1, '#DE9733']]),  ## animal rights
         dimensions = list([
@@ -57,26 +57,26 @@ layout = go.Layout(
 )
 
 labels = [
-        dict(xref='paper', yref='paper', x=0.35, y=-0.1,
+        dict(xref='paper', yref='paper', x=0.35, y=-0.085,
                               xanchor='center', yanchor='top',
                               text="Alex's Lemonade Stand",
                               font=dict(family='Arial',
                                         size=14,
-                                        color='#B71752'),
+                                        color='#AA1B13'),
                               showarrow=False),
-        dict(xref='paper', yref='paper', x=0.5, y=-0.1,
+        dict(xref='paper', yref='paper', x=0.5, y=-0.085,
                               xanchor='center', yanchor='top',
                               text="National Immigration Law Center",
                               font=dict(family='Arial',
                                         size=14,
-                                        color='#CAD11A'),
+                                        color='#6C9E12'),
                               showarrow=False),
-        dict(xref='paper', yref='paper', x=0.65, y=-0.1,
+        dict(xref='paper', yref='paper', x=0.65, y=-0.085,
                               xanchor='center', yanchor='top',
                               text="Doctor's Without Borders",
                               font=dict(family='Arial',
                                         size=14,
-                                        color='#6EBE18'),
+                                        color='#0D5F67'),
                               showarrow=False)
                               
         ]
